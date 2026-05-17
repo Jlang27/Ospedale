244c0c71662f05b39cf2e461cafc6bcf4169948a
Jlang27 <joseluisangulom27@gmail.com>
2026-05-17 15:53:05 -0500

    feat: Empezando la estrucutra MVC

--- src/core/models/Hospitalization.java
+++ src/core/models/Hospitalization.java
@@ -2,9 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package packagee;
+package core.models;
 
+import core.models.Doctor;
 import java.time.LocalDate;
+import packagee.HospitalizationStatus;
+import packagee.Patient;
+import packagee.RoomType;
 
 /**
  *
