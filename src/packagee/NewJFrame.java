37d2acf797b65c694a1e9cbaef037050065e959d
Jlang27 <joseluisangulom27@gmail.com>
2026-05-17 14:08:19 -0500

    feat: El archivo ahora se ejecuta desde Main.java

--- src/packagee/NewJFrame.java
+++ src/packagee/NewJFrame.java
@@ -467,21 +467,7 @@ public class NewJFrame extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        System.setProperty("flatlaf.useNativeLibrary", "false");
-
-        try {
-            UIManager.setLookAndFeel(new FlatDarkLaf());
-        } catch (Exception ex) {
-            System.err.println("Failed to initialize LaF");
-        }
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new NewJFrame().setVisible(true);
-            }
-        });
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
