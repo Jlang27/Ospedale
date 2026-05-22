c83e849d84f9f0cb57372804006d50f69926da5e
Jlang27 <joseluisangulom27@gmail.com>
2026-05-22 11:04:03 -0500

    Clase Response y otras cosas

--- src/core/controllers/Response.java
+++ src/core/controllers/Response.java
@@ -9,5 +9,26 @@ package core.controllers;
  * @author josel
  */
 public class Response {
+    private final int statusCode;
+    private final String message;
+    private final Object data;
     
+    
+    public Response(int statusCode, String message, Object data){
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Object getData() {
+        return data;
+    }
 }
